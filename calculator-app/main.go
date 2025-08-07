c9266e4029a4038d66c7636387ce2d53e57e4e0c
Calum Rees <calumrees99@gmail.com>
2025-08-07 11:18:27 +0100

    basic calculator functionality

---
+++ calculator-app/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	var number1 int
+	var number2 int
+
+	fmt.Println("input first number:")
+	number1 = numberInput()
+
+	fmt.Println("input second number:")
+	number2 = numberInput()
+
+	add(number1, number2)
+	subtract(number1, number2)
+	multiply(number1, number2)
+	divide(number1, number2)
+}
+
+func numberInput() int {
+
+	var number int
+	_, err := fmt.Scan(&number)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return (number)
+}
+
+func add(number1 int, number2 int) {
+	result := number1 + number2
+	fmt.Printf("Result: %d\n", result)
+}
+
+func subtract(number1 int, number2 int) {
+	result := number1 - number2
+	fmt.Printf("Result: %d\n", result)
+}
+
+func multiply(number1 int, number2 int) {
+	result := number1 * number2
+	fmt.Printf("Result: %d\n", result)
+}
+
+func divide(number1 int, number2 int) {
+	result := number1 / number2
+	fmt.Printf("Result: %d\n", result)
+}

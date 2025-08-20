2b8108da01fee29348d7d9d9f74fab02f71ae733
Calum Rees <calumrees99@gmail.com>
2025-08-20 21:54:40 +0100

    added create & get functionality

---
+++ crud-app/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type User struct {
+	Name string `json:"name"`
+}
+
+var userCache = make(map[int]User)
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /users/{id}", getUser)
+	mux.HandleFunc("POST /users", createUser)
+	mux.HandleFunc("DELETE /users", deleteUser)
+
+	// Start server
+	fmt.Println("Server is listening on :8080")
+	http.ListenAndServe(":8080", mux)
+}
+
+func getUser(
+	w http.ResponseWriter,
+	r *http.Request) {
+
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user, ok := userCache[id]
+	if !ok {
+		http.Error(w, "user not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application.json")
+	j, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+func createUser(
+	w http.ResponseWriter,
+	r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if user.Name == "" {
+		http.Error(w, "name is required", http.StatusBadRequest)
+		return
+	}
+
+	userCache[len(userCache)+1] = user
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func deleteUser(
+	w http.ResponseWriter,
+	r *http.Request) {
+}
